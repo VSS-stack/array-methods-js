918b0e7304cecbed6d75207ea931bf1a2a4947aa
Victor Santos Silva <victor.stsilva93@gmail.com>
2023-03-19 12:38:11 -0300

    including the number zero after the price

--- app/main.js
+++ app/main.js
@@ -20,7 +20,7 @@ function exibirLivrosNaTela(listaDeLivros) {
             <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}"/>
             <h2 class="livro__titulo">${livro.titulo}</h2>
             <p class="livro__descricao">${livro.autor}</p>
-            <p class="livro__preco" id="preco">R$${livro.preco.toString().replace('.', ',')}</p>
+            <p class="livro__preco" id="preco">R$${livro.preco.toString().replace('.', ',')}0</p>
             <div class="tags">
                 <span class="tag">${livro.categoria}</span>
             </div>
